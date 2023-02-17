a543d5d7e69a2f8a48614aedb5affb087716a34a
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-17 13:05:30 +0300

    Lowercase Then Uppercase then newline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - MAin Function
+ *
+ * Description: prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Pass)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+
+	return (0);
+}
