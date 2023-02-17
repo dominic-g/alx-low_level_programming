9278bd4ee9fe76a04d5fa8c767008e52310c7c73
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-17 13:32:20 +0300

    Hexadecimal numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Main Function
+ *
+ * Description: program that prints all the numbers of base 16
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Pass)
+*/
+
+int main(void)
+{
+	int digit = 48; 
+
+	while (digit <= 102) 
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
