e61b91f3bcc3d2bf444d8f2b81579930c1361c7b
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-17 13:44:53 +0300

    Advanced Task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,8 @@
 /**
  * main - Main Function
  *
- * Description: program that prints all possible different combinations of two digits
+ * Description: program that prints all possible different
+ * combinations of two digits
  *
  * Return: Always 0 (Pass)
 */
