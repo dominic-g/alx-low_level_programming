6c26a02d8fc56fe63cf56cf05ea11401886d7484
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 17:17:57 +0300

    Betty correction

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /*
-*The compilation command requires this file to be present
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ *         On error, -1 is returned, and errno is set appropriately.
 */
 
 int _putchar(char ch)
