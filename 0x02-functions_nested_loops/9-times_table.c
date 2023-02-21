3a6494c0da3d59bad48c47f0b01b5e36615e73f6
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 15:39:10 +0300

    Times Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/*
+*Times  Table
+*
+*/
+void times_table(void){
+	int row , column;
+
+	for(row = 0;row < 10; ++row){
+		for(column = 0; column < 10; ++column){
+
+			int prd = row * column;
+
+			if(column > 0){
+				if(prd < 10)
+					_putchar(' ');
+				_putchar(' ');
+			}
+			if(prd >= 10)	
+				_putchar((prd / 10) + 48);
+			_putchar((prd % 10) + 48);
+
+			if(column > 8){
+				_putchar('\n');
+			}else{
+				_putchar(',');
+			}
+		}
+	}
+}
