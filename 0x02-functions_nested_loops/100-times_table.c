001e7d33472554d24277b7a139e5ec89224a94bf
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 16:32:31 +0300

    Advance Times Table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/*
+*Advanced Times table
+*/
+
+void print_times_table(int n){
+	if(n >= 0 && n <= 15){
+
+
+		int row, column;
+
+		for(row = 0; row <= n; ++row){
+
+
+			for(column = 0; column <= n; ++column){
+				int prd = row * column;
+				int prd_ = row * column;
+
+				if(column > 0){
+					if(prd >= 100){
+						_putchar(' ');
+					}else if(prd >= 10){
+						_putchar(' ');
+						_putchar(' ');
+					}else{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+
+				if(prd >= 100){
+					_putchar( (prd / 100)+48);
+					prd -= (prd / 100) * 100;
+				}
+
+				if(prd >= 10){
+					_putchar( (prd / 10) + 48);
+					prd -= (prd / 10) * 10;
+				}else if(prd_ >= 100){
+					_putchar(48);
+				}
+
+				_putchar( (prd + 48));
+				if(column != n)
+					_putchar(',');
+				else
+					_putchar('\n');
+
+
+
+
+
+			}
+
+		}
+
+	}
+}
