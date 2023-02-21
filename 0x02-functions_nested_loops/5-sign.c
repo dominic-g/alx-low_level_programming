db19a9540391cddc43705d28b15d92b6b680e5e8
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 20:32:43 +0300

    No 5 coreection for output

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,15 @@ int print_sign(int n)
 
 	if (n < 0)
 	{
-		_putchar(43 + 0);
+		_putchar(45 + 0);
 		x = -1;
 	} else if (n > 0)
 	{
-		_putchar(48 + 0);
+		_putchar(43 + 0);
 		x = 1;
 	} else
 	{
-		_putchar(45 + 0);
+		_putchar(48 + 0);
 		x = 0;
 	}
 
