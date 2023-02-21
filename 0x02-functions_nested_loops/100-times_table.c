c5b1182f6ea5b076b82693a6659e03b691a5b07e
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 20:53:03 +0300

    Further betty pain for more than 40 lines in a function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,56 +20,64 @@ void print_times_table(int n)
 		{
 			for (column = 0; column <= n; ++column)
 			{
-				int prd = row * column;
-				int prd_ = row * column;
-
-				if (column > 0)
-				{
-					if (prd >= 100)
-					{
-						_putchar(' ');
-					}
-					else if (prd >= 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-
-				if (prd >= 100)
-				{
-					_putchar((prd / 100) + 48);
-					prd -= (prd / 100) * 100;
-				}
-
-				if (prd >= 10)
-				{
-					_putchar((prd / 10) + 48);
-					prd -= (prd / 10) * 10;
-				}
-				else if (prd_ >= 100)
-					_putchar(48);
-
-				_putchar((prd + 48));
-				if (column != n)
-					_putchar(',');
-				else
-					_putchar('\n');
-
-
-
-
-
+				additional(row, column, n);
 			}
 
 		}
 
 	}
+}
+
+/**
+ * additional - Advanced Times table
+ *
+ * @row: integer
+ * @column: integer
+ * @n: integer 
+ *
+ * Description: check the code.
+ *
+ * Return: Always 0 (Success)
+*/
+void additional (int row, int column, int n){
+	int prd = row * column;
+	int prd_ = row * column;
+	
+	if (column > 0)
+	{
+		if (prd >= 100)
+		{
+			_putchar(' ');
+		}
+		else if (prd >= 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+
+	if (prd >= 100)
+	{
+		_putchar((prd / 100) + 48);
+		prd -= (prd / 100) * 100;
+	}
+	if (prd >= 10)
+	{
+		_putchar((prd / 10) + 48);
+		prd -= (prd / 10) * 10;
+	}
+	else if (prd_ >= 100)
+		_putchar(48);
+	_putchar((prd + 48));
+	if (column != n)
+		_putchar(',');
+	else
+		_putchar('\n');
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,5 +17,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void additional (int row, int column, int n);
 
 #endif
