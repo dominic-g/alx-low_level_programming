01e79edc8b506196565c969bcb11bfe8e5e48a7a
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-02-21 15:13:21 +0300

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+*Jack Bauer minutes
+*
+*/
+
+void jack_bauer(void){
+	int hour, minute;
+
+	for(hour =  0; hour < 24; ++hour){
+
+		for(minute = 0; minute < 60; ++minute){
+
+			_putchar( (hour / 10) + 48);
+			_putchar( (hour % 10) + 48);
+
+			_putchar(':');
+
+			_putchar( (minute / 10) + 48);
+			_putchar( (minute % 10) + 48);
+			_putchar('\n');
+
+		}
+
+	}	
+}
