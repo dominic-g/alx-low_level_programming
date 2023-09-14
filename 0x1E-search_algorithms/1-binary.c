95917148adf568f86fc0cfaea8d1404c45bb64ea
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-09-13 22:07:12 -0400

    Betty 1 binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,26 +13,26 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left_idx, right_idx;
+	size_t left_idx, right_idx, i, mid_idx;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (left_idx = 0, right_idx = size - 1; right_idx >= left_idx;)
-    {
-        printf("Searching in array: ");
-        for (size_t i = left_idx; i < right_idx; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[right_idx]);
+	for (left_idx = 0, right_idx = size - 1; right_idx >= left_idx;)
+	{
+		printf("Searching in array: ");
+		for (i = left_idx; i < right_idx; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[right_idx]);
 
-        size_t mid_idx = left_idx + (right_idx - left_idx) / 2;
-        if (array[mid_idx] == value)
-            return (int)mid_idx;
-        if (array[mid_idx] > value)
-            right_idx = mid_idx - 1;
-        else
-            left_idx = mid_idx + 1;
-    }
+		mid_idx = left_idx + (right_idx - left_idx) / 2;
+		if (array[mid_idx] == value)
+			return ((int)mid_idx);
+		if (array[mid_idx] > value)
+			right_idx = mid_idx - 1;
+		else
+			left_idx = mid_idx + 1;
+	}
 
-    return (-1);
+	return (-1);
 }
