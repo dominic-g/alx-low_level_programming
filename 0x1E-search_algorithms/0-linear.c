60802f72c48d57c5f0ac5f512995a655df9b5cfa
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-09-13 21:36:20 -0400

    Files

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * linear_search - Search for a value in an array using linear search.
+ * @array: Pointer to the first element of the array.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is found, return the index of the first occurrence;
+ * otherwise, return -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+
+    for (j = 0; j < size; j++)
+    {
+        printf("Value checked array[%u] = [%d]\n", j, array[j]);
+        if (array[j] == value)
+            return (int)j;
+    }
+
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches value in sorted array of integers.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, return -1.
+ *         Otherwise, return the index where the value is located.
+ *
+ * Description: Prints the [sub]array being searched after each change.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    size_t left_idx, right_idx;
+
+    if (array == NULL)
+        return (-1);
+
+    for (left_idx = 0, right_idx = size - 1; right_idx >= left_idx;)
+    {
+        printf("Searching in array: ");
+        for (size_t i = left_idx; i < right_idx; i++)
+            printf("%d, ", array[i]);
+        printf("%d\n", array[right_idx]);
+
+        size_t mid_idx = left_idx + (right_idx - left_idx) / 2;
+        if (array[mid_idx] == value)
+            return (int)mid_idx;
+        if (array[mid_idx] > value)
+            right_idx = mid_idx - 1;
+        else
+            left_idx = mid_idx + 1;
+    }
+
+    return (-1);
+}
