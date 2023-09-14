05b8d42ddb8c6a8a280c5df23fe9217005e67424
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-09-13 22:46:13 -0400

    2 file(s)  edited: .gitignore 102-interpolation.c Pushed at 2023-09-...

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,21 +1,18 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - Search for a value in a sorted array
- *			of integers using Interpolation search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
  *
- * Return: If the value is not present or the array is NULL, return -1.
- *         Otherwise, return the index where the value is located.
- *
- * Description: Prints the elements being checked during the search.
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t position, low, high;
-	double factor;
+	size_t pos, low, high;
+	double f;
 
 	if (array == NULL)
 		return (-1);
@@ -23,35 +20,33 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
-	while (size > 0)
+	while (size)
 	{
-		factor = (high - low) / (array[high] - array[low]) * (value - array[low]);
-		position = (size_t)(low + factor);
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
 
-		printf("Value checked array[%d]", (int)position);
-
-		if (position >= size)
+		if (pos >= size)
 		{
 			printf(" is out of range\n");
 			break;
 		}
 		else
 		{
-			printf(" = [%d]\n", array[position]);
+			printf(" = [%d]\n", array[pos]);
 		}
 
-		if (array[position] == value)
-			return ((int)position);
+		if (array[pos] == value)
+			return ((int)pos);
 
-		if (array[position] < value)
-			low = position + 1;
+		if (array[pos] < value)
+			low = pos + 1;
 		else
-			high = position - 1;
+			high = pos - 1;
 
 		if (low == high)
 			break;
-		}
+	}
 
 	return (-1);
 }
-
